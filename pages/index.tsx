3d61cd8cff50d0a04360d2d380e1c39d485361bc
faznaa <faznaaharees@gmail.com>
2023-10-31 23:34:10 +0530

    added meta description

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,21 @@ import { Unbounded } from 'next/font/google'
 import Hero from './components/Hero'
 import Description from './components/Description'
 import Projects from './components/Projects'
+import Head from 'next/head'
 
 const inter = Unbounded({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div className={`${inter.className}`}>
+      <Head>
+        <meta name='title' content='Fazna Harees - Fullstack Developer' />
+        <meta name="description"
+        content='Need your website ready? I can help you with that. I am a fullstack developer based in Bangalore, Kerala, IN. I specialize in creating responsive websites, landing pages and AI Chatbots.' />
+        
+
+
+      </Head>
       <Hero />
       <Description />
       <Projects />
