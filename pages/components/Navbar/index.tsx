9bc597af3b56a78653cb4afb08f8d9a067fd8309
faznaa <faznaaharees@gmail.com>
2023-12-30 19:57:31 +0530

    scroll added

--- pages/components/Navbar/index.tsx
+++ pages/components/Navbar/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { motion, useReducedMotion } from 'framer-motion'
+import { useRouter } from 'next/router'
 
 export default function Navbar() {
     const items = [
@@ -28,11 +29,19 @@ export default function Navbar() {
             link:'/contact'
         },
     ]
+    const router = useRouter()
+    const scroll =(name:any) => {
+        if(name.startsWith('/')){
+            router.push(name)
+        }else {
+        document.querySelector(name).scrollIntoView({ behavior: 'smooth' });
+        }
+    }
   return (
     <div className='fixed top-0 left-0 w-full z-50 bg-white shadow-md'>
         <div className='flex flex-row justify-center items-center px-4 sm:px-10 py-4'>
             <div className='flex flex-row gap-x-8 items-center'>
-                {items.map((item,index)=><a  href={item.link} className='text-lg sm:text-xl font-semibold tracking-tight text-black hover:text-indigo-700'>{item.name}</a>)}
+                {items.map((item,index)=><button onClick={() => scroll(item.link)}  className='text-lg sm:text-xl font-semibold tracking-tight text-black hover:text-indigo-700'>{item.name}</button>)}
             </div>
             </div>
         </div>
