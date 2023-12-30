ce17b75e74b3abb37f0414c81a5888f2b409ff00
faznaa <faznaaharees@gmail.com>
2023-12-30 19:54:24 +0530

    added skills

--- pages/components/Navbar/index.tsx
+++ pages/components/Navbar/index.tsx
@@ -32,7 +32,7 @@ export default function Navbar() {
     <div className='fixed top-0 left-0 w-full z-50 bg-white shadow-md'>
         <div className='flex flex-row justify-center items-center px-4 sm:px-10 py-4'>
             <div className='flex flex-row gap-x-8 items-center'>
-                {items.map((item,index)=><motion.a  href={item.link} className='text-lg sm:text-xl font-semibold tracking-tight text-black'>{item.name}</motion.a>)}
+                {items.map((item,index)=><a  href={item.link} className='text-lg sm:text-xl font-semibold tracking-tight text-black hover:text-indigo-700'>{item.name}</a>)}
             </div>
             </div>
         </div>
